8eb204945f03bccdbf35c1a9302404140f11ae64
Jean-Baptiste Lavisse <jean-baptiste@jblavisse.fr>
2021-04-28 17:24:40 +0200

    Add exercise 4-5 and 4-6

---
+++ js/exo4-5.js
@@ -0,0 +1,8 @@
+/*
+4.5 Ecrire un algorithme qui affiche 
+les nombres de 5 en 5 de 123 jusqu'à 356
+*/
+
+for(let nb=123; nb<=356; nb=nb+5) {
+    console.log(nb);
+}
---
+++ js/exo4-6.js
@@ -0,0 +1,13 @@
+
+/*
+4.6 Ecrire un algorithme qui décompte 
+les nombre de 60 à 1, puis dit "Bonne année"
+*/
+// i++ -> i=i+1
+// i-- -> i=i-1
+
+for(let i=60; i>=1;i--) {
+    console.log(i);
+}
+
+console.log("C'est la fin du cours o/")
